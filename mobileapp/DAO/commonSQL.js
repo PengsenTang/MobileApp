9ca0dbdb08993ea9ee4324bee3f09cb3e5315309
sen <491469748@qq.com>
2018-05-21 23:59:18 +0800

    Remove unnecessary SQL

--- mobileapp/DAO/commonSQL.js
+++ mobileapp/DAO/commonSQL.js
@@ -2,7 +2,6 @@
 var userinfo = {
 	user_list:'select id from user_info',
     all:'select * from user_info where id=?',
-    getpassword:'select* from authentication where id = ?'
     get_all_info:'select * from user_info where id=?',
     get_info:'select ?? from user_info where id=?',
     update_info:'update user_info set ??=? where id=?',
@@ -13,7 +12,7 @@ var users = {
 	create_authentication:'insert into authentication values(?,?)',
 	email_register:'insert into user_info(mail_address,name,gender,register_time) values(?,?,?,?)',
 	getIdByNumber:'select * from user_info where phone_number = ?',
-	getIdByEmail:'select * from user_info where mail_address = ? ',
+	getIdByEmail:'select * from user_info where mail_address = ?',
 	check_authentication:'select password from authentication where user_id = ?',
 	whetherNumberRegistered:'select * from user_info where phone_number = ?',
 	whetherEmailRegistered:'select * from user_info where mail_address = ?'
