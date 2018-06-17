6f568a7ef10835bf9fad82c6bac86f0a7e5dcab7
sen <491469748@qq.com>
2018-06-17 22:04:45 +0800

    Reorganise SMS to avoid submodule problem

---
+++ mobileapp/SMS/sendMessage.js
@@ -0,0 +1,36 @@
+/**
+ * 云通信基础能力业务短信发送、查询详情以及消费消息示例，供参考。
+ * Created on 2017-07-31
+ */
+const SMSClient = require('@alicloud/sms-sdk')
+// ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
+const accessKeyId = 'LTAInqFwvaSp5XSf'
+const secretAccessKey = 'dQmpcStlhRk8PUgZQHqNQr2WSWwlVr'
+//初始化sms_client
+let smsClient = new SMSClient({accessKeyId, secretAccessKey})
+//发送短信
+
+
+function messageVerify(phoneNumber,verifyCode){
+    smsClient.sendSMS({
+        PhoneNumbers: phoneNumber,
+        SignName: '小世界',
+        TemplateCode: 'SMS_137665616',
+        TemplateParam: JSON.stringify({
+            "code":verifyCode
+        })
+    }).then(function (res) {
+        let {Code}=res
+        if (Code === 'OK') {
+            //处理返回参数
+            console.log(res)
+        }
+    }, function (err) {
+        console.log(err)
+    })
+}
+
+// messageVerify('15082362189',"666999")
+module.exports={
+    messageVerify:messageVerify
+}
